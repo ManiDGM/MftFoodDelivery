e4900179c9b7c274d39ef3574eb345cc4c42d2bd
saha computer <roudgarmobina@gmail.com>
2023-12-29 02:46:13 +0330

    da

--- model/entity/food_order.py
+++ model/entity/food_order.py
@@ -1,15 +1,22 @@
 from model.entity.base import Base
-from sqlalchemy import Integer,String,Float,Column,Boolean,Date,DateTime
+from sqlalchemy import Integer,Column,Boolean,DateTime
 
 class Food_Order(Base):
     __tableame__="foodorder_tbl"
 
-
     id=Column(Integer,primary_key=True)
     customer_id=Column(Integer)
-    order_status_id=Column(Boolean)
-    order_datetime=Column(DateTime)
-    total_amount=Column(Float)
+    status=Column(Boolean)
+    datetime=Column(DateTime)
+#    total_amount=Column(Float)
 
+    def __init__(self,id,customer_id,status,datetime,total_amount):
+        self.id=id
+        self.customer_id=customer_id
+        self.status=status
+        self.datetime=datetime
+        self.total_amount=total_amount
 
+    def __repr__(self):
+        return str(self.__dict__)
 
