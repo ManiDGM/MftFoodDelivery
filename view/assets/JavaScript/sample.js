eb48f34369698a20c26e1f49d0b40676a6e1dcf6
ManiDGM <manidgm1383@gmail.com>
2023-12-31 21:29:23 +0330

    view updated ManiDGM

--- view/assets/JavaScript/sample.js
+++ view/assets/JavaScript/sample.js
@@ -11,5 +11,57 @@ openShopping.addEventListener('click' , () =>{
 })
 
 closeShopping.addEventListener('click' , () =>{
-    bidy.classList.remove('active');
+    body.classList.remove('active');
 })
+
+let products = [
+    {
+        id: 1,
+        name: 'PRODUCT NAME 1' ,
+        image: 'burger.png',
+        price: 50
+    },
+    {
+        id: 2,
+        name: 'PRODUCT NAME 2' ,
+        image: 'coffee.png',
+        price: 20
+    },
+    {
+        id: 3,
+        name: 'PRODUCT NAME 3' ,
+        image: 'pizza.png',
+        price: 60
+    },
+    {
+        id: 4,
+        name: 'PRODUCT NAME 4' ,
+        image: 'salad.png',
+        price: 30
+    },
+    {
+        id: 5,
+        name: 'PRODUCT NAME 5' ,
+        image: 'seafood.png',
+        price: 80
+    },
+    {
+        id: 6,
+        name: 'PRODUCT NAME 6' ,
+        image: 'soup.png',
+        price: 20
+    },
+];
+let listCards = [];
+function initApp(){
+   products.forEach((value,key) => {
+       let newDiv = document.createElement('div');
+       newDiv.innerHTML = `
+            <img src="/view/assets/img/${value.image}"/>
+            <div class="title">${value.name}></div>
+            <div class="price">${value.price.toLocaleString()}></div>
+       `;
+       list.appendchild(newDiv);
+   })
+}
+initApp();
