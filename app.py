11068d6c95d37c776ae53b553184b262426749eb
ManiDGM <manidgm1383@gmail.com>
2024-01-08 23:34:28 +0330

    app updated ManiDGM

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, redirect, request, session
 from controller import *
 from flask_session import Session
 
-app = Flask(__name__, template_folder="view", static_folder="view/assets")
+app = Flask(__name__, template_folder="view", static_folder="view/static")
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_PERMANENT"] = "filesystem"
 Session(app)
@@ -22,6 +22,25 @@ def login():
         password = request.form.get("password")
 
         data = CustomerController.login(email, password)
+
+
+
+@app.route("/signup", methods=["POST", "GET"])
+def signup():
+    if request.method == "POST":
+        status, data = CustomerController.save(
+            request.form.get("first_name"),
+            request.form.get("last_name"),
+            request.form.get("email"),
+            request.form.get("password"))
+
+    return render_template("signup.html")
+
+
+
+@app.route("/forget")
+def forget():
+    return render_template("forget-password.html")
 
 
 app.run()
